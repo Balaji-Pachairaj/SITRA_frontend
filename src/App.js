95ffdf96fe7143d37ff620d2c827706a34039ef9
Balaji <balajitheblankde@gmail.com>
2024-12-14 20:16:53 +0530

    feat: install Router-dom, redux toolkit, tailwind CSS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/home/HomePage";
+import { Provider } from "react-redux";
+import { store } from "./store";
+
+const router = createBrowserRouter([{ path: "/", element: <HomePage /> }]);
+
 function App() {
-  return <div>Frontend Started</div>;
+  return (
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  );
 }
 
 export default App;
